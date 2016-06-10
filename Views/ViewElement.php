ae6b2618b0e4b6828576e20d89f195ef70bdb884
Ioannis Papikas <papikas.ioan@gmail.com>
2016-06-10 02:04:16 +0200

    Updating default values.

--- Views/ViewElement.php
+++ Views/ViewElement.php
@@ -39,7 +39,7 @@ class ViewElement extends HTMLElement
      *
      * @throws Exception
      */
-    public function __construct($HTMLDocument, $view, $name, $value = "", $id = "", $class = "")
+    public function __construct($HTMLDocument, $view, $name = "div", $value = "", $id = "", $class = "")
     {
         // Create DOMItem
         parent::__construct($HTMLDocument, $name, $value, $id, $class);
