0da3b8573dfe1f923011c85ce2cb912bf619c97f
Ioannis Papikas <papikas.ioan@gmail.com>
2016-06-13 17:39:43 +0200

    initial travis setup and init code for tests.

---
+++ tests/init.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Initialize the application autoloader to take care of all
+ * automatic class loads through the entire application.
+ */
+require(__DIR__ . '/../vendor/autoload.php');
+
+date_default_timezone_set('UTC');
