c2aad3bd1a4fb62ec0e3370e56db0158e5ca6b04
Renato <renatobylyk@gmail.com>
2018-11-08 10:46:20 +0200

    Add check to avoid replacing '&' when building async-get data

--- Html/HTMLElement.php
+++ Html/HTMLElement.php
@@ -234,7 +234,7 @@ class HTMLElement extends DOMItem
         foreach ($data['data'] as $name => $value) {
             // Evaluate given value using scss syntax (replace & with existing value)
             $existingValue = $this->getHTMLHandler()->data($element, $name);
-            $value = str_replace('&', $existingValue, $value);
+            $value = $name != 'async-get' ? str_replace('&', $existingValue, $value) : $value;
 
             // Set new value
             $this->getHTMLHandler()->data($element, $name, $value);
